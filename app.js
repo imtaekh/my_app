c69e269921a754ec0b39b2fb6c97c69f3a5d2ee4
imtaekh <imtaek@gmail.com>
2015-09-01 10:02:47 -0700

    static folder setting

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/',function (req,res) {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function(){
   console.log('Server On!');
