9eefb7446bf8e7d665397c34faafb39a822fc344
imtaekh <imtaek@gmail.com>
2015-10-22 16:55:13 -0700

    Post model added, Post API added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // connect database
 mongoose.connect(process.env.MONGO_DB);
@@ -15,14 +16,53 @@ db.on("error",function (err) {
 });
 
 // model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
+});
+var Post = mongoose.model('post',postSchema);
 
 // view setting
 app.set("view engine", 'ejs');
 
 // set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 // set routes
+app.get('/posts', function(req,res){
+  Post.find({}, function (err,posts) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:posts});
+  });
+}); // index
+app.post('/posts', function(req,res){
+  Post.create(req.body.post,function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
+  });
+}); // create
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
+  });
+}); // show
+app.put('/posts/:id', function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" updated"});
+  });
+}); //update
+app.delete('/posts/:id', function(req,res){
+  Post.findByIdAndRemove(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" deleted"});
+  });
+}); //destroy
 
 // start server
 app.listen(3000, function(){
