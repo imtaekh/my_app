be31544796a3ad04f933de22cc86f7d846bf7e72
imtaekh <imtaek@gmail.com>
2016-01-22 20:25:07 -0800

    Author added to the board

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ db.on("error",function (err) {
 var postSchema = mongoose.Schema({
   title: {type:String, required:true},
   body: {type:String, required:true},
+  author: {type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
   createdAt: {type:Date, default:Date.now},
   updatedAt: Date
 });
@@ -189,44 +190,53 @@ app.put('/users/:id', isLoggedIn, checkUserRegValidation, function(req,res){
 
 // set posts routes
 app.get('/posts', function(req,res){
-  Post.find({}).sort('-createdAt').exec(function (err,posts) {
+  Post.find({}).populate("author").sort('-createdAt').exec(function (err,posts) {
     if(err) return res.json({success:false, message:err});
     res.render("posts/index", {data:posts, user:req.user});
   });
 }); // index
-app.get('/posts/new', function(req,res){
-  res.render("posts/new");
+app.get('/posts/new', isLoggedIn, function(req,res){
+  res.render("posts/new", {user:req.user});
 }); // new
-app.post('/posts', function(req,res){
-  console.log(req.body);
+app.post('/posts', isLoggedIn, function(req,res){
+  req.body.post.author=req.user._id;
   Post.create(req.body.post,function (err,post) {
     if(err) return res.json({success:false, message:err});
     res.redirect('/posts');
   });
 }); // create
 app.get('/posts/:id', function(req,res){
-  Post.findById(req.params.id, function (err,post) {
+  Post.findById(req.params.id).populate("author").exec(function (err,post) {
     if(err) return res.json({success:false, message:err});
-    res.render("posts/show", {data:post});
+    res.render("posts/show", {data:post, user:req.user});
   });
 }); // show
-app.get('/posts/:id/edit', function(req,res){
+app.get('/posts/:id/edit', isLoggedIn, function(req,res){
   Post.findById(req.params.id, function (err,post) {
     if(err) return res.json({success:false, message:err});
-    res.render("posts/edit", {data:post});
+    if(!req.user._id.equals(post.author)) return res.json({success:false, message:"Unauthrized Attempt"});
+    res.render("posts/edit", {data:post, user:req.user});
   });
 }); // edit
-app.put('/posts/:id', function(req,res){
+app.put('/posts/:id', isLoggedIn, function(req,res){
   req.body.post.updatedAt=Date.now();
-  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post) {
+  Post.findById(req.params.id, function (err,post) {
     if(err) return res.json({success:false, message:err});
-    res.redirect('/posts/'+req.params.id);
+    if(!req.user._id.equals(post.author)) return res.json({success:false, message:"Unauthrized Attempt"});
+    Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post) {
+      if(err) return res.json({success:false, message:err});
+      res.redirect('/posts/'+req.params.id);
+    });
   });
 }); //update
-app.delete('/posts/:id', function(req,res){
-  Post.findByIdAndRemove(req.params.id, function (err,post) {
+app.delete('/posts/:id', isLoggedIn, function(req,res){
+  Post.findById(req.params.id, function (err,post) {
     if(err) return res.json({success:false, message:err});
-    res.redirect('/posts');
+    if(!req.user._id.equals(post.author)) return res.json({success:false, message:"Unauthrized Attempt"});
+    Post.findByIdAndRemove(req.params.id, function (err,post) {
+      if(err) return res.json({success:false, message:err});
+      res.redirect('/posts');
+    });
   });
 }); //destroy
 
